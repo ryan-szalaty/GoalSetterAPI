099af3be3e6691767f423ac7e0b36056ff63d7d2
ザラテイ・ライアン <szalatyryan@zarateiraians-MacBook-Air.local>
2022-04-18 13:52:17 +0900

    Completed API CRUD operations.

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,9 +1,10 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../model/goalModel');
+const User = require('../model/userModel');
 
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({user: req.user.id});
     res.status(200).json({goals: goals});
 });
 
@@ -12,7 +13,10 @@ const setGoals = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Please add a text field.');
     }
-    const goal = await Goal.create({text: req.body.text})
+    const goal = await Goal.create({
+        text: req.body.text,
+        user: req.user.id
+    });
     res.status(200).json({message: `Goal created: ${goal}`});
 });
 
@@ -22,6 +26,15 @@ const updateGoals = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Goal not found. Please check the ID.');
     };
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(401);
+        throw new Error('Unauthorized');
+    }
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('Unauthorized');
+    }
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
     res.status(200).json({message: `Goal updated: ${updatedGoal}`});
 });
@@ -32,6 +45,15 @@ const deleteGoals = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Goal not found. Please check the ID.');
     };
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(401);
+        throw new Error('Unauthorized');
+    }
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('Unauthorized');
+    }
     const deletedGoal = await Goal.findByIdAndDelete(req.params.id);
     res.status(200).json({message: `Deleted goal ${deletedGoal}`});
     console.log('Goal deleted.'.red.underline);
---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../model/userModel');
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields.');
+    }
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists.');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    });
+    console.log('User successfully registered.'.magenta);
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error('Invalid user data.');
+    }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+        console.log('Successful login!'.blue);
+    }
+    else {
+        res.status(400).json({
+            message: 'Authentication invalid.'
+        });
+        console.log('Failed login.'.red);
+    }
+});
+
+const getUser = asyncHandler(async (req, res) => {
+    const {name, email, _id} = await User.findById(req.user.id);
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    });
+    console.log('User obtained'.yellow);
+});
+
+const deleterUser = asyncHandler(async (req, res) => {
+    res.json({message: 'Delete user.'});
+});
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getUser,
+    deleterUser
+}
